1a903f11b52d95a002a1079a362185accab2e6a1
Гаврилова Иван <discover1977@yandex.ru>
2016-11-12 22:02:10 +0300

    Add lib files

---
+++ HTSensor.c
@@ -0,0 +1,163 @@
+#include "HTSensor.h"
+#include <util/delay.h>
+
+enum HTDataName
+{
+	MSBData,
+	LSBData,
+	CRCData
+};
+
+static unsigned char UserRegData;
+
+static unsigned char CalcSht21Crc(unsigned char *data,unsigned char nbrOfBytes)
+{
+	unsigned char byteCtr,bit,crc;
+	crc = 0;
+	//calculates 8-Bit checksum with given polynomial
+	for (byteCtr = 0; byteCtr < nbrOfBytes; ++byteCtr)
+	{
+		crc ^= (data[byteCtr]);
+		for (bit = 8; bit > 0; --bit)
+		{
+			if (crc & 0x80) crc = (crc << 1) ^ POLYNOMIAL;
+				else 		crc = (crc << 1);
+		}
+	}
+	return(crc);
+}
+
+unsigned char HTReadUserReg()
+{
+	// Read user register data
+	i2c_Soft_Start();
+	i2c_Soft_Write(HT_WR_ADDR);
+	i2c_Soft_Write(HT_READ_USER_REGISTER);
+	i2c_Soft_Start();
+	i2c_Soft_Write(HT_RD_ADDR);
+	UserRegData = i2c_Soft_Read(0);
+	i2c_Soft_Stop();
+	return UserRegData;
+}
+void HTWriteUserReg(unsigned char value)
+{
+	// Write user register data
+	i2c_Soft_Start();
+	i2c_Soft_Write(HT_WR_ADDR);
+	i2c_Soft_Write(HT_WRITE_USER_REGISTER);
+	i2c_Soft_Write(value);
+	i2c_Soft_Stop();
+}
+
+void HTSensorHeatOn()
+{
+	UserRegData |= (1 << 2);
+
+	i2c_Soft_Start();
+	i2c_Soft_Write(HT_WR_ADDR);
+	i2c_Soft_Write(HT_WRITE_USER_REGISTER);
+	i2c_Soft_Write(UserRegData);
+	i2c_Soft_Stop();
+}
+
+void HTSensorHeatOff()
+{
+	UserRegData &= (~(1 << (2)));
+
+	i2c_Soft_Start();
+	i2c_Soft_Write(HT_WR_ADDR);
+	i2c_Soft_Write(HT_WRITE_USER_REGISTER);
+	i2c_Soft_Write(UserRegData);
+	i2c_Soft_Stop();
+}
+
+void HTSensorReset() {
+	i2c_Soft_Start();
+	i2c_Soft_Write(HT_WR_ADDR);
+	i2c_Soft_Write(HT_SOFT_RESET);
+	i2c_Soft_Stop();
+	_delay_ms(15);
+	// Read user register data
+	UserRegData = HTReadUserReg();
+}
+
+float HTSensorReadTemp() {
+	unsigned char Data[3], Ack;
+	Ack = 0;
+
+	i2c_Soft_Start();
+	i2c_Soft_Write(HT_WR_ADDR);
+	i2c_Soft_Write(HT_TEMP_MEAS_noHOLD_MASTER);
+
+	do {
+		i2c_Soft_Start();
+		Ack = i2c_Soft_Write(HT_RD_ADDR);
+	} while(!Ack);
+
+	Data[MSBData] = i2c_Soft_Read(1);
+	Data[LSBData] = i2c_Soft_Read(1);
+	Data[CRCData] = i2c_Soft_Read(0);
+	i2c_Soft_Stop();
+    
+	// process data
+	if(Data[CRCData] == CalcSht21Crc(Data, 2)) {
+	    unsigned int temp = Data[MSBData];
+	    temp = temp << 8;
+	    temp = temp | (Data[LSBData] & 0xFC);
+	    float tempC = -46.85 + 175.72*((float)temp/65536.0);
+	    return tempC;
+	}
+	else {
+		return 9999;
+	}
+}
+
+float HTSensorReadHumidity() {
+	unsigned char Data[3], Ack;
+
+	i2c_Soft_Start();
+	i2c_Soft_Write(HT_WR_ADDR);
+	i2c_Soft_Write(HT_HUM_MEAS_noHOLD_MASTER);
+
+	do {
+		i2c_Soft_Start();
+		Ack = i2c_Soft_Write(HT_RD_ADDR);
+	} while(!Ack);
+
+	Data[MSBData] = i2c_Soft_Read(1);
+	Data[LSBData] = i2c_Soft_Read(1);
+	Data[CRCData] = i2c_Soft_Read(0);
+	i2c_Soft_Stop();
+
+	// process data
+	if(Data[CRCData] == CalcSht21Crc(Data, 2)) {
+	    unsigned int rh = Data[MSBData];
+	    rh = rh << 8;
+	    rh = rh | (Data[LSBData] & 0xFC);
+	    float rhfloat = -6 + 125 * (float)rh / 65536;
+	    return rhfloat;
+	}
+	else {
+		return 0;
+	}
+}
+
+float HTSensorReadCompensatedHumidity(void)
+{
+	float humidity;
+	float temperature;
+	float compensatedHumidity;
+
+	humidity    = HTSensorReadHumidity();
+	temperature = HTSensorReadTemp();
+
+	if (humidity < 0 ) return (0);
+	if (humidity > 100 ) return (100);
+
+	if (temperature > 0 && temperature < 80)
+	{
+		return compensatedHumidity = humidity + (25 - temperature) * HT_TEMP_COEFFICIENT;
+	}
+
+	return humidity;
+}
---
+++ HTSensor.h
@@ -0,0 +1,65 @@
+/* 
+ * File:   HTU21DF.h
+ * Author: Jason McGuire (j.mcguire.2015@ieee.org)
+ * Description: A library for a HTU21D-F Humidity/Temperature Sensor
+ * for the PIC18F using I2C.
+ * See example.c for implementation of the library
+ * Sample Product: https://www.adafruit.com/products/1899
+ * Created on March 27, 2016, 9:08 AM
+ 
+License Information:
+This is free and unencumbered software released into the public domain.
+
+Anyone is free to copy, modify, publish, use, compile, sell, or
+distribute this software, either in source code form or as a compiled
+binary, for any purpose, commercial or non-commercial, and by any
+means.
+
+In jurisdictions that recognize copyright laws, the author or authors
+of this software dedicate any and all copyright interest in the
+software to the public domain. We make this dedication for the benefit
+of the public at large and to the detriment of our heirs and
+successors. We intend this dedication to be an overt act of
+relinquishment in perpetuity of all present and future rights to this
+software under copyright law.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+ 
+ */
+
+#ifndef HTU21DF_H
+#define HTU21DF_H
+
+#include "i2c-soft.h"
+
+#define HT_WR_ADDR 					0x80 // write address for temp/humidity sensor
+#define HT_RD_ADDR 					0x81 // read address for temp/humidity sensor
+
+#define HT_TEMP_MEAS_HOLD_MASTER	0xE3
+#define HT_HUM_MEAS_HOLD_MASTER		0xE5
+#define HT_TEMP_MEAS_noHOLD_MASTER	0xF3
+#define HT_HUM_MEAS_noHOLD_MASTER	0xF5
+#define HT_WRITE_USER_REGISTER		0xE6
+#define HT_READ_USER_REGISTER		0xE7
+#define HT_SOFT_RESET				0xFE
+#define HT_TEMP_COEFFICIENT			-0.15
+
+#define POLYNOMIAL  0x0131
+
+// Available Functions:
+float HTSensorReadTemp(); // Get a temperature reading
+float HTSensorReadHumidity(); // Get a humidity reading
+float HTSensorReadCompensatedHumidity(void);
+void HTSensorReset(); // Soft reset of sensor 
+void HTSensorHeatOn();
+void HTSensorHeatOff();
+unsigned char HTReadUserReg();
+void HTWriteUserReg(unsigned char);
+
+#endif // HTU21DF_H
